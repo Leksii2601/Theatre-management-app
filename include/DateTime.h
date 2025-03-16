20ad341ea3834cf986e5313c4b68d29f476c8ed2
Oleksii Dashchuk <oleksii.dashchuk@lnu.edu.ua>
2025-03-16 19:37:46 +0200

    Create DateTime.h

---
+++ include/DateTime.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <ctime>
+
+struct DateTime {
+    int day;
+    int month;
+    int year;
+    int hour;
+    int minute;
+    
+    bool operator==(const DateTime& other) const;  // перевіряє чи дві дати однакові
+    bool operator<(const DateTime& other) const;   // порівнює дві дати, щоб визначити яка з них раніше
+    bool isOnSameDay(const DateTime& other) const; // перевіряє чи дві дати належать одному дню
+    bool isFuture() const;                         // перевіряє чи дата знаходиться в майбутньому
+};
