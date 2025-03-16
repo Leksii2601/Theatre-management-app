a3461f325c02b67f15b7b6b5e6e6ca50af92ef1b
Oleksii Dashchuk <oleksii.dashchuk@lnu.edu.ua>
2025-03-16 20:57:45 +0200

    Update Ticket.h

--- include/Ticket.h
+++ include/Ticket.h
@@ -2,15 +2,17 @@
 #include <fstream>
 #include <iostream>
 using namespace std;
-extern const int MAX_NAME_LENGTH;
+
+// Define MAX_NAME_LENGTH with a specific value
+#define MAX_NAME_LENGTH 100
 
 struct Ticket {
     int id;
     int performanceIndex;                 // індекс вистави
-    char buyerName[MAX_NAME_LENGTH];
-    int seatNumber;
+    char buyerName[MAX_NAME_LENGTH];      // ім'я покупця
+    int seatNumber;                       // номер місця
     bool sold;                           // чи продано квиток
-    
+
     Ticket();
     void saveToFile(std::ofstream& file) const;
     bool loadFromFile(std::ifstream& file);
